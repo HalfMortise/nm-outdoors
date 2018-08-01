849662772dd2e4ac44f8d842c5acad758536c45a
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-01 14:41:33 -0600

    Added some doc blocs

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -83,6 +83,7 @@ class Profile {
          $exceptionType = get_class($exception);
          throw(new $exceptionType($exception->getMessage(), 0, $exception));
       }
+      //end exception
    }
-
+   //end constructor
 }
