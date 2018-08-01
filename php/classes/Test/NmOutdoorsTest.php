808273fe9ee3a1a8a096bac127d5e1d18d5cab66
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-01 13:37:12 -0600

    Changed .ini to nmoutdoors to reflect current project

--- php/classes/Test/NmOutdoorsTest.php
+++ php/classes/Test/NmOutdoorsTest.php
@@ -83,8 +83,8 @@ abstract class NmOutdoorsTest extends TestCase {
       // if the connection hasn't been established, create it
       if($this->connection === null) {
          // connect to mySQL and provide the interface to PHPUnit
-         $config = readConfig("/etc/apache2/capstone-mysql/ddctwitter.ini");
-         $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/ddctwitter.ini");
+         $config = readConfig("/etc/apache2/capstone-mysql/nmoutdoors.ini");
+         $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/nmoutdoors.ini");
          $this->connection = $this->createDefaultDBConnection($pdo, $config["database"]);
       }
       return($this->connection);
