ab9c13bd2825a593087b0af4e9d6fde99929c2f0
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-02 15:58:15 -0600

    Commit all, attempting to resolve autoload issue

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,7 +1,7 @@
 <?php
 namespace HalfMortise\NmOutdoors;
-require_once("autoload.php"); //autoload.php file in Classes directory
-require_once(dirname(__DIR__, 2) . "../vendor/autoload.php"); //composer-generated autoload
+require_once("autoload.php");
+require_once(dirname(__DIR__, 3) . "../vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
 /**
  * Small cross section of a web application for outdoor enthusiasts that enables users
---
+++ php/classes/Test/ProfileTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace HalfMortise\NmOutdoors;
+require_once("../classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+use Ramsey\Uuid\Uuid;
+
+
+/**
+ *
+ *
+ *
+ *
+ */
