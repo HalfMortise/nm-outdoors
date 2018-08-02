ca3dc75df8fab7ccd45cd55d4070367a68eca102
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-02 16:05:29 -0600

    Updated composer autoload dir. path

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -1,13 +1,12 @@
 <?php
 namespace HalfMortise\NmOutdoors;
-require_once("../classes/autoload.php");
-require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once("../autoload.php");
+require_once(dirname(__DIR__, 3) . "../../vendor/autoload.php");
 use Ramsey\Uuid\Uuid;
-
+use HalfMortise\NmOutdoors\Profile
 
 /**
- *
- *
- *
+ * This is the PHPUnit test for the class Profile
  *
  */
+
