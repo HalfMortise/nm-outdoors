87443ae10e0800fa3f7f13617a1bb9d37ed116e0
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-02 20:53:05 -0600

    added public function 'testInsert'

--- php/classes/Test/ActivityTest.php
+++ php/classes/Test/ActivityTest.php
@@ -22,6 +22,36 @@ class ActivityTest extends NmOutdoorsTest {
 	 * valid name to create the profile object to own the test
 	 * @var string $VALID_NAME
 	 */
-	protected $VALID_NAME;
+	protected $VALID_NAME= "PHPUnit First Test";
+	/**
+	 * Name of the Activity
+	 * @var string $VALID_NAME2
+	 **/
+	protected $VALID_NAME2 = "PHPUnit test passing";
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp()  : void {
+		// run the default setUp() method first
+		parent::setUp();
+	}
 
+	/**
+	 * test inserting a valid Name and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidActivity() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("activity");
+
+		// create a new Activity and insert to into mySQL
+		$activityId = generateUuidV4();
+		$activity = new Activity($activityId, $this->VALID_NAME);
+		$activity->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoActivity = Activity::getActivityByActivityId($this->getPDO(), $activity->getActivityId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("activity"));
+		$this->assertEquals($pdoActivity->getActivityId(), $activityId);
+		$this->assertEquals($pdoActivity->getActivityName(), $this->VALID_NAME);
+	}
 }
