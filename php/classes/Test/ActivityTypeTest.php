104d2169073d13f6b8ae1856f2abb1fcd82606ea
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-05 17:25:15 -0600

    added test insert and test delete https://app.asana.com/0/762061287654589/764118250103785

--- php/classes/Test/ActivityTypeTest.php
+++ php/classes/Test/ActivityTypeTest.php
@@ -7,7 +7,7 @@ use HalfMortise\NmOutdoors\Test\{ActivityType, Activity, RecArea};
 require_once(dirname(__DIR__) . "/autoload.php");
 
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "../lib/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  * Full PHPUnit test for the ActivityType class
@@ -39,7 +39,47 @@ class ActivityTypeTest extends NmOutdoorsTest {
 		//run the default setUp() method first
 		parent::setUp();
 
-		//create and insert the mocked profile
-		$this->activity = new Activity(generateUuidV4(), null, "@phpunit",)
+		//create and insert the mocked Activity
+		$this->activity = new Activity(generateUuidV4(), null, "@phpunit",);
+		$this->activity->insert($this->getPDO());
 	}
+
+	/**
+	 * test inserting a valid ActivityType and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidActivityType(): void {
+		//count the number od rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("activityType");
+
+		//create a new activityType and insert it into mySQL
+		$activityType = new ActivityType($this->recArea->getRecAreaId(), $this->activity->getActivityId);
+		$activityType->insert($this->getPDO());
+
+		//grab the data from mySQL and enfore the fields match our expectations
+		$pdoActivityType = ActivityType::getActivityTypeByActivityTypeActivityIdAndActivityTypeRecAreaId($this->getPDO(), $this->activity->getActivityId(), $this->recArea->getRecAreaId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("activityType"));
+		$this->assertEquals($pdoActivityType->getActivityTypeActivityId(), $this->activity->getActivityId());
+		$this->assertEquals($pdoActivityType->getActivityTypeRecAreaId(), $this->recArea->getRecAreaId());
+	}
+	/**
+	 * test creating a valid activityType and then deleting it
+	 **/
+	public function testDeleteValidActivityType(): void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("activityType");
+
+		//create a new ActivityType and insert it into mySQL
+		$activityType = new ActivityType($this->activity->getActivityId(), $this->recArea->getRecAreaId());
+		$activityType->insert($this->getPDO());
+
+		//delete the activityType from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("activityType"));
+		$activityType->delete($this->getPDO());
+
+		//grab the data from mySQL and enforce the ActivityType does not exist
+		$pdoActivityType = ActivityType::getActivityTypeByActivityTypeActivityIdAndActivityTypeRecAreaId($this->getPDO(), $this->activity->getActivityId(), $this->recArea->getRecAreaId());
+		$this->assertNull($pdoActivityType);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("activityType"));
+	}
+
 }
