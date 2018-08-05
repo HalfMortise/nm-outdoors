3d0d6b5e66cd239d1be5bec252abdc1ef8baa1b8
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-05 17:32:05 -0600

    added test insert and test delete https://app.asana.com/0/762061287654589/764118250103785

--- php/classes/Test/ActivityTest.php
+++ php/classes/Test/ActivityTest.php
@@ -7,7 +7,7 @@ use HalfMortise\NmOutdoors\Activity;
 require_once(dirname(__DIR__) . "/autoload.php");
 
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "../lib/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  * Full PHPUnit test for the Activity class
