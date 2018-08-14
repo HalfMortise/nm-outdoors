53d9cae6319688d5b12e255ee86fece3f1fc3ea5
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-14 15:51:49 -0600

    added start session segment https://app.asana.com/0/762061287654589/778007676828788

--- public_html/api/sign-in/index.php
+++ public_html/api/sign-in/index.php
@@ -5,3 +5,24 @@ require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 use HalfMortise\NmOutdoors\Profile;
 
+/**
+ * api for handling sign-in
+ *
+ * @author Gkephart
+ **/
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
+	//grab mySQL statement
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/nmoutdoors.ini");
+
+	//determine which HTTP method is being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
