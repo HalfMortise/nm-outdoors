e44e1b91a8c1ae11fcc7fd7b2fe383c7ef3b6faf
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-14 15:23:01 -0600

    Updated with exception throws

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -43,11 +43,37 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
       $activation = filter_input(INPUT_GET, "activation", FILTER_SANITIZE_STRING);
       //make sure the activation token is the correct length
       if(strlen($activation) !== 32){
-         throw(new InvalidArgumentException("activation token is an incorrect length", 405));
+         throw(new \InvalidArgumentException("activation token is an incorrect length", 405));
       }
       //verify that the activation token is a string value of a hexadecimal
       if(ctype_xdigit($activation) === false) {
          throw (new \InvalidArgumentException("activation is empty or has invalid contents", 405));
+      }
+      //handle the GET HTTP request
+      if($method === "GET") {
+         //set XSRF cookie
+         setXsrfCookie();
+         //find the profile associated with the activation token
+         $profile = Profile::getProfileByProfileActivationToken($pdo, $activation);
+         //verify that the profile is not null
+         if($profile !== null) {
+            //make sure the activation token matches
+            if($activation === $profile->getProfileActvationToken()) {
+               //set activation to null
+               $profile->setProfileActivationToken(null);
+               //update the profile in the database
+               $profile->update($pdo);
+               //set the reply for the end user
+               $reply->data = "Thank you for activating your account; you will now be redirected to your profile.";
+            } else {
+               //throw an exception if the activation token does not exist
+               throw(new \RuntimeException("Profile with this activation token does not exist", 404));
+            } else {
+               //throw an exception if the HTTP request is not a GET
+               throw(new \InvalidArgumentException("Invalid HTTP method request", 403));
+            }
+            //
+         }
       }
    }
 
