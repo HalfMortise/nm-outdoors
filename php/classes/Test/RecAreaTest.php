c56ba9995227883c6b500cabc83be7bd90d691a2
James R. Lambert <ryolambert@gmail.com>
2018-08-14 10:02:25 -0600

    erased TODO comments at bottom of class

--- php/classes/Test/RecAreaTest.php
+++ php/classes/Test/RecAreaTest.php
@@ -319,11 +319,4 @@ public function testGetAllRecAreas() : void {
 	$this->assertEquals($pdoRecArea->getRecAreaName(),$this->VALID_RECAREANAME);
 
 }
-/**
- * TODO
- * 1. testGetValidRecAreaByDistance
- * 2. testGetInvalidRecAreaByDistance
- * 3. testGetValidRecAreaByRecAreaName
- * 4. testGetInvalidRecAreaByRecAreaName
- */
 }
