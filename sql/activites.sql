2701d3449f3c1647fd30269cf218b0a35cac6f14
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-22 14:25:02 -0600

    added activies.sql for api download

---
+++ sql/activites.sql
@@ -0,0 +1,7 @@
+INSERT INTO activity(activityId, activityName) VALUES (0xc7f124df7fb149cc931a195dbbf72f26, "Biking");
+INSERT INTO activity(activityId, activityName) VALUES (0xbd4f0d59bad9422caabe3f78f26a64cb, "Camping");
+INSERT INTO activity(activityId, activityName) VALUES (0x0716f3c577d34d5f900cd99d2b170ae9, "Fishing");
+INSERT INTO activity(activityId, activityName) VALUES (0xa0bfba5270914f31a9b6e3a3334e9d4e, "Hiking");
+INSERT INTO activity(activityId, activityName) VALUES (0x00d01746ae144960806ef059850f3e16, "Wildlife Viewing");
+INSERT INTO activity(activityId, activityName) VALUES (0xc2b984b7a683491e8842b36248b15104, "Water Sports");
+INSERT INTO activity(activityId, activityName) VALUES (0xfecd38dc24d14552a3025ac3859441ca, "Winter Sports");
