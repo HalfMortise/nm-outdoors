0acaf4839206bcb20dad5187c943aa14f17b8167
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-25 15:48:55 -0600

    began directory structure for angular

---
+++ src/vendor.ts
@@ -0,0 +1,19 @@
+// Angular 6
+import "@angular/platform-browser";
+import "@angular/playform-browser-dynamic";
+import "@angular/core";
+import "@angular//common";
+import "@angular/router";
+
+//RxJS
+import "rxjs";
+
+//jquery and bootstrap
+import "jquery";
+import "bootstrap";
+
+//font awesome
+import "@fontawesome/fontawesome";
+import "@fontawesome/fontawesome-free-brands";
+import "@fontawesome/fontawesome-free-regular";
+import "@fontawesome/fontawesome-free-solid";
