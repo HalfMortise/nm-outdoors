ac80be6abfd6af29c309f64e10e19e681ea8aaab
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-28 16:55:01 -0600

    Added doc blocs

--- src/app/shared/interfaces/sign.in.ts
+++ src/app/shared/interfaces/sign.in.ts
@@ -1,1 +1,9 @@
-...
+/******************************************************************************************************/
+/*	Module name: sign.in.ts																										*/
+/* Module description: SInterface for user to access web app profile sign up service						*/
+/*	Author: HalfMortise																											*/
+/*	Date: 8/--/2018																												*/
+/******************************************************************************************************/
+
+/* Interface */
+
