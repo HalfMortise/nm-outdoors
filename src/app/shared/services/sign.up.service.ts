a8050c1a70c84df728263f07527d149ac908c3cf
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-28 16:34:08 -0600

    Updated with doc blocs

--- src/app/shared/services/sign.up.service.ts
+++ src/app/shared/services/sign.up.service.ts
@@ -1,10 +1,23 @@
+/******************************************************************************************************/
+/*	Module name: sign.up.service.ts																							*/
+/* Module description: Service to allow a user to create an account in the web application				*/
+/*	Author: HalfMortise																											*/
+/*	Date: 8/28/2018																												*/
+/******************************************************************************************************/
+
+/* Imports */
+
 import {Injectable} from "@angular/core";
 import {Status} from "../interfaces/status";
 import {Observable} from "rxjs/internal/Observable";
 import {SignUp} from "../interfaces/sign.up";
 import {HttpClient} from "@angular/common/http";
 
+/* Injectable metadata */
+
 @Injectable()
+
+/* Service */
 
 export class SignUpService {
 
