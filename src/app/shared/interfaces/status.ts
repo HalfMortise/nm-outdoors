2fe2fd82af2a90d8692aab28362b91ccc3bfae03
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-28 16:53:34 -0600

    Added doc blocs

--- src/app/shared/interfaces/status.ts
+++ src/app/shared/interfaces/status.ts
@@ -1,3 +1,12 @@
+/******************************************************************************************************/
+/*	Module name: status.ts																										*/
+/* Module description: Interface to allow for any given status to be communicated to user					*/
+/*	Author: HalfMortise																											*/
+/*	Date: 8/28/2018																												*/
+/******************************************************************************************************/
+
+/* Interface */
+
 export interface Status {
 	status: number,
 	message: string,
