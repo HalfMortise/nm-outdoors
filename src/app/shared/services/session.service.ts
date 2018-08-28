850dea1c66733f49844784e2c7f9f044d807bfac
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-28 15:56:55 -0600

    Drafted sign-up service

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -1,1 +1,5 @@
-...
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@an"
+
+
+<Board>(this.boardUrl, {params: new HttpParams().set("boardProfileId", boardProfileId)})
--- src/app/shared/services/sign.up.service.ts
+++ src/app/shared/services/sign.up.service.ts
@@ -1,1 +1,18 @@
-...
+import {Injectable} from "@angular/core";
+import {Status} from "../interfaces/status";
+import {Observable} from "rxjs/internal/Observable";
+import {SignUp} from "../interfaces/sign.up";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class SignUpService {
+	constructor(protected http: HttpClient) {
+
+	}
+
+	private signUpUrl = "api/sign-up/";
+
+	createProfile(signUp: SignUp) : Observable<Status> {
+		return(this.http.post<Status>(this.signUpUrl, signUp));
+	}
+}
