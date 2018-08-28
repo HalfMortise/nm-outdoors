d360ccdc9cbd72cb03fcd8ba5dfe2607fa7c3e56
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-28 16:52:34 -0600

    Added doc blocs

--- src/app/shared/interfaces/sign.up.ts
+++ src/app/shared/interfaces/sign.up.ts
@@ -1,3 +1,12 @@
+/******************************************************************************************************/
+/*	Module name: sign.up.ts																										*/
+/* Module description: Interface for user to access web app profile sign up service							*/
+/*	Author: HalfMortise																											*/
+/*	Date: 8/28/2018																												*/
+/******************************************************************************************************/
+
+/* Interface */
+
 export interface SignUp {
 	profileAtHandle: string,
 	profileEmail: string,
