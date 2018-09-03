fe5c97356d57b28f82f32caf4b1be92eb93e6b69
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 11:59:16 -0600

    Added to doc block with instructions for plugins

--- static-ui/sign-in-up-modal.php
+++ static-ui/sign-in-up-modal.php
@@ -4,6 +4,8 @@
  * to either sign up for a profile or sign into a profile.
  * It will contain two tabs: Left will be for sign-in,
  * Right will be for sign-up
+ *
+ * This component will require the use of Bootstrap 4, Popper, and JQuery (see comments at bottom of page)
  */
 -->
 
@@ -157,25 +159,8 @@
 		</div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		<!--JQuery, Popper, and Bootstrap JS plugins-->
+		<!--Insert these tags before closing <body> tag: JQuery, Popper, and Bootstrap JS plugins
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	</body>
-</html>
+		-->
