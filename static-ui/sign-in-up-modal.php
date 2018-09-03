d25d0395e81fd5b8b0bbea5e2f09099d73477635
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 09:35:11 -0600

    Collecting form field input snippets: added password

--- static-ui/sign-in-up-modal.php
+++ static-ui/sign-in-up-modal.php
@@ -5,3 +5,34 @@
  * It will contain two tabs: Left will be for sign-up,
  * Right will be for sign-in
  */
+
+<section>
+   <div class="modal fade" id="sign-in-up-modal" tabindex="-2" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+     <div class="modal-dialog modal-dialog-centered" role="document">
+       <div class="modal-content">
+         <div class="modal-header">
+           <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+             <span aria-hidden="true">&times;</span>
+           </button>
+         </div>
+         <div class="modal-body">
+           ...
+         </div>
+         <div class="modal-footer">
+           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+           <button type="button" class="btn btn-primary">Sign Up</button>
+         </div>
+       </div>
+     </div>
+   </div>
+</section>
+
+
+
+//password field
+<label for="inputPassword5">Password</label>
+<input type="password" id="password" class="form-control" aria-describedby="passwordHelpBlock">
+<small id="passwordHelpBlock" class="form-text text-muted">
+Your password must be 8-20 characters long, contain letters, numbers, and special characters.
+</small>
