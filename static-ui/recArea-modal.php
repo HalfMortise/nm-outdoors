d721fec7de11b6eff446166a3b27e6c99e501483
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-03 10:40:08 -0600

    added sample text for modal

---
+++ static-ui/recArea-modal.php
@@ -0,0 +1,15 @@
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div id="image-review">
+				<img src="">
+				<p class="review-section">Reviews reviews reviews!</p>
+			</div>
+			<div id="recAreaDescription">
+				<p>Rec Area Description! It's amazing! You should totally go visit!</p>
+			</div>
+		</div>
+	</div>
+</div>
