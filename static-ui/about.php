ad678b247689bfa72825ba4fd5d0bc52a87bd690
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 13:46:45 -0600

    Added card deck with two cards; added simple header (will likely remove later)

--- static-ui/about.php
+++ static-ui/about.php
@@ -6,3 +6,31 @@
  * This component will require the use of Bootstrap 4 and Popper
  */
 -->
+
+<h3 class="about-h3">About the Developers</h3>
+
+<div class="card-deck text-center">
+
+	<div class="card">
+		<img class="card-img-top" src="./images/placeholder.jpg" alt="sh-profile-img">
+		<div class="card-body">
+			<h5 class="card-title">Sarah</h5>
+			<p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+		</div>
+		<div class="card-footer">
+			<small class="text-muted">TheCodingCowgirl.com</small>
+		</div>
+	</div>
+
+	<div class="card">
+		<img class="card-img-top" src="./images/placeholder.jpg" alt="rl-profile-img">
+		<div class="card-body">
+			<h5 class="card-title">Ryo</h5>
+			<p class="card-text">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.</p>
+		</div>
+		<div class="card-footer">
+			<small class="text-muted">Ryo.com</small>
+		</div>
+	</div>
+
+</div>
