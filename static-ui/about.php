98f742ced11b3991a648200e259e58a135a145ff
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 13:23:29 -0600

    Adding 'about' component with doc block

---
+++ static-ui/about.php
@@ -0,0 +1,8 @@
+<!--/**
+ * this component is the About page which will contain
+ * a brief description of the developers and the purpose
+ * of the development of the application.
+ *
+ * This component will require the use of Bootstrap 4 and Popper
+ */
+-->
