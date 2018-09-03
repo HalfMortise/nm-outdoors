479545516425e0324c462ca6301dbafca8e318b7
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-03 14:45:33 -0600

    began work on the profile page with the reviews left

---
+++ static-ui/review-view.php
@@ -0,0 +1,22 @@
+<section>
+	<div id="profile">
+		<h3>Profile</h3>
+		<ul>
+			<li>UserName</li>
+			<li>Name</li>
+			<li>Email</li>
+		</ul>
+		<img src="" alt="profile image for user">
+	</div>
+	<div>
+		<h3>My Reviews</h3>
+		<ul>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+		</ul>
+	</div>
+</section>
