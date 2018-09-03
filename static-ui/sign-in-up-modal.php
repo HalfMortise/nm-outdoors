3b7fee19f76cae17ec814914469efa68ceb5a7cc
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 11:31:07 -0600

    Replaced snippets; completed form within modal. Still working on getting signin tab functional

--- static-ui/sign-in-up-modal.php
+++ static-ui/sign-in-up-modal.php
@@ -2,42 +2,137 @@
 /**
  * this component is the modal that will "pop up" when a user elects
  * to either sign up for a profile or sign into a profile.
- * It will contain two tabs: Left will be for sign-up,
- * Right will be for sign-in
+ * It will contain two tabs: Left will be for sign-in,
+ * Right will be for sign-up
  */
 
-<section>
-   <div class="modal fade" id="sign-in-up-modal" tabindex="-2" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-     <div class="modal-dialog modal-dialog-centered" role="document">
-       <div class="modal-content">
-         <div class="modal-header">
-           <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-             <span aria-hidden="true">&times;</span>
-           </button>
-         </div>
-         <div class="modal-body">
-           ...
-         </div>
-         <div class="modal-footer">
-           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-           <button type="button" class="btn btn-primary">Sign Up</button>
-         </div>
-       </div>
-     </div>
-   </div>
-</section>
+
+<div class="container">
+			<hr />
+			<br />
+				<h3>Please sign in</h3>
+				<br>
+				<a class="btn btn-primary btn-lg" href="#signin" data-toggle="modal" data-target=".bs-modal-sm">Sign In/Sign Up</a>
+			<br />
+			<hr />
+		</div>
+
+
+		<!-- Modal -->
+		<div class="modal fade bs-modal-sm" id="myModal" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-sm">
+				<div class="modal-content">
+					<br>
+					<div class="bs-example bs-example-tabs">
+						<div id="nav-tab" class="nav nav-tabs" role="tablist">
+
+							<a class="nav-item nav-link active" href="#signin" id="signin" data-toggle="tab" role="tab" aria-controls="nav-signin" aria-selected="true">Sign In</a>
+							<a class="nav-item nav-link" href="#signup" data-toggle="tab" role="tab" aria-controls="nav-signup" aria-selected="true">Sign Up</a>
+
+						</div>
+					</div>
+					<div class="modal-body">
+						<div id="myTabContent" class="tab-content">
+							<div class="tab-pane fade active in" id="signin">
+								<form class="form-horizontal">
+									<fieldset>
+										<!-- Sign In Form -->
+										<!-- Text input-->
+										<div class="control-group">
+											<label class="control-label" for="at-handle">Name:</label>
+											<div class="controls">
+												<input id="at-handle" type="text" class="form-control input-medium" placeholder="" required="required">
+											</div>
+										</div>
+
+										<!-- Password input-->
+										<div class="control-group">
+											<label class="control-label" for="password">Password:</label>
+											<div class="controls">
+												<input required="required" id="password" class="form-control input-medium" type="password" placeholder="********">
+											</div>
+										</div>
+
+
+										<!-- Button -->
+										<div class="control-group">
+											<label class="control-label" for="signin"></label>
+											<div class="controls">
+												<button id="signin" name="signin" class="btn btn-success">Sign In</button>
+											</div>
+										</div>
+									</fieldset>
+								</form>
+							</div>
+							<div class="tab-pane fade" id="signup">
+								<form class="form-horizontal">
+									<fieldset>
+										<!-- Sign Up Form -->
+										<!-- Text input-->
+
+										<!-- Text input-->
+										<div class="control-group">
+											<label class="control-label" for="userid">Username:</label>
+											<div class="controls">
+												<input id="userid" name="userid" class="form-control" type="text" placeholder="" required="required">
+											</div>
+										</div>
+
+										<div class="control-group">
+											<label class="control-label" for="Email">Email:</label>
+											<div class="controls">
+												<input id="email" class="form-control" type="text" placeholder="" required="required">
+											</div>
+										</div>
+
+
+										<!-- Password input-->
+										<div class="control-group">
+											<label class="control-label" for="password">Password:</label>
+											<div class="controls">
+												<input id="password" name="password" class="form-control" type="password" placeholder="********" required="required">
+											</div>
+										</div>
+
+										<!-- Text input-->
+										<div class="control-group">
+											<label class="control-label" for="reenterpassword">Re-Enter Password:</label>
+											<div class="controls">
+												<input id="reenterpassword" class="form-control" type="password" placeholder="********" required="required">
+											</div>
+										</div>
+
+										<!-- Multiple Radios (inline) -->
+										<!-- Replace with reCAPTCHA? -->
+										<br>
+										<div class="control-group">
+											<label class="control-label" for="humancheck">Humanity Check:</label>
+											<div class="controls">
+												<label class="radio inline" for="humancheck-0">
+													<input type="radio" name="humancheck" id="humancheck-0" value="robot" checked="checked">I'm a Robot</label>
+												<label class="radio inline" for="humancheck-1">
+													<input type="radio" name="humancheck" id="humancheck-1" value="human">I'm Human</label>
+											</div>
+										</div>
+
+										<!-- Button -->
+										<div class="control-group">
+											<label class="control-label" for="confirmsignup"></label>
+											<div class="controls">
+												<button id="confirmsignup" name="confirmsignup" class="btn btn-success">Sign Up</button>
+											</div>
+										</div>
+									</fieldset>
+								</form>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
 
-//password field
-<form class="form-inline">
-  <div class="form-group">
-    <label for="inputPassword6">Password</label>
-      <input type="password" id="password" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-
-    <label for="inputPassword6">Confirm password</label>
-      <input type="password" id="password" class="form-control mx-sm-3" aria-describedby="passwordHelpInline">
-         <small id="passwordHelpInline" class="text-muted">Must be 8-20 characters long.</small>
-  </div>
-</form>
