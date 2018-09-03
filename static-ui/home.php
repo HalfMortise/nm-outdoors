391abfb7e6d351724fb3b97bd538cb9dcd6032f4
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-03 14:29:50 -0600

    Adding Home component along with doc block

---
+++ static-ui/home.php
@@ -0,0 +1,12 @@
+<!--
+/**
+ * this component is the landing page that will provide the
+ * user with a brief description of the application as well as
+ * the option to search for activities (by activity), to sign up
+ * for an account, to sign into an account, or to navigate to
+ * other features (such as the map or browse rec areas)
+ *
+ * This component will require the use of Bootstrap 4, Popper, and JQuery (see comments at bottom of page)
+ */
+-->
+
