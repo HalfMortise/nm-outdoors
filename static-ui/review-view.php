2db8fa73b6ec1c090f717c851083b5a72be6c45b
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 14:43:06 -0500

    updated review-view.php page

--- static-ui/review-view.php
+++ static-ui/review-view.php
@@ -2,15 +2,6 @@
 <?php require_once ("navbar.php");?>
 
 <section>
-	<div id="profile">
-		<h3>Profile</h3>
-		<ul>
-			<li>UserName</li>
-			<li>Name</li>
-			<li>Email</li>
-		</ul>
-		<img src="" alt="profile image for user">
-	</div>
 	<div id="review">
 		<h3>My Reviews</h3>
 		<ul>
