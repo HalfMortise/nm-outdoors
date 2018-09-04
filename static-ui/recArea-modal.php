9c3c79894f68ec9b635caf6ba4bed423aa4fbc17
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-03 21:05:19 -0600

    fixed the modal titles and then added id's to the review view

--- static-ui/recArea-modal.php
+++ static-ui/recArea-modal.php
@@ -2,16 +2,16 @@
 <?php require_once ("navbar.php");?>
 
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+<button type="button" class="btn" data-toggle="modal" data-target="#recAreaModal">
 	Launch demo modal
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<div class="modal fade" id="recAreaModal" tabindex="-1" role="dialog" aria-labelledby="recAreaModalTitle" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="exampleModalLongTitle">Rec Area</h5>
+				<h5 class="modal-title" id="recAreaModalTitle">Rec Area</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
--- static-ui/review-view.php
+++ static-ui/review-view.php
@@ -1,3 +1,6 @@
+<?php require_once ("head-utils.php");?>
+<?php require_once ("navbar.php");?>
+
 <section>
 	<div id="profile">
 		<h3>Profile</h3>
@@ -8,7 +11,7 @@
 		</ul>
 		<img src="" alt="profile image for user">
 	</div>
-	<div>
+	<div id="review">
 		<h3>My Reviews</h3>
 		<ul>
 			<li>Review goes here</li>
