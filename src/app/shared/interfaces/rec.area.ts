699c6cd940b96cff0e1c8b302052b12722d41306
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-03 21:36:13 -0500

    added rec area interface

--- src/app/shared/interfaces/rec.area.ts
+++ src/app/shared/interfaces/rec.area.ts
@@ -1,1 +1,11 @@
-...
+export interface RecArea {
+	recAreaId: string;
+	recAreaDescription: string;
+	recAreaDirections: string;
+	recAreaImageUrl: string;
+	recAreaLat: string;
+	recAreaLong: string;
+	recAreaMapUrl: string;
+	recAreaName: string;
+
+}
