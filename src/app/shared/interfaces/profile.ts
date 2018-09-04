08df7c17831fd603f0d7b1d9d5a767e81fbb5b47
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-03 21:30:03 -0500

    added profile interface

--- src/app/shared/interfaces/profile.ts
+++ src/app/shared/interfaces/profile.ts
@@ -1,1 +1,9 @@
-...
+export interface Profile {
+	profileId: string;
+	profileActivationToken: string;
+	profileAtHandle: string;
+	profileEmail:string;
+	profileHash: string;
+	profileImageUrl:string;
+}
+
