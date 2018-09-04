0970df9f1a9810a64d99bc918eb5c81b0f99e8b4
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 15:26:48 -0500

    more updates to review-view.php. Linked it to explore page

--- static-ui/review-view.php
+++ static-ui/review-view.php
@@ -4,13 +4,25 @@
 <section>
 	<div id="review">
 		<h3>My Reviews</h3>
-		<ul>
-			<li>Review goes here</li>
-			<li>Review goes here</li>
-			<li>Review goes here</li>
-			<li>Review goes here</li>
-			<li>Review goes here</li>
-			<li>Review goes here</li>
-		</ul>
+
+			<p>Review 1 goes here
+				<a href="explore-page.php">RecArea 1</a>
+			</p>
+			<p>Review 2 goes here
+				<a href="explore-page.php">RecArea 2</a>
+		   </p>
+			<p>Review 3 goes here
+				<a href="explore-page.php">RecArea 3</a>
+			</p>
+			<p>Review 4 goes here
+				<a href="explore-page.php">RecArea 4</a>
+			</p>
+			<p>Review 5 goes here
+				<a href="explore-page.php">RecArea 5</a>
+			</p>
+			<p>Review 6 goes here
+				<a href="explore-page.php">RecArea 6</a>
+			</p>
+
 	</div>
 </section>
