ca2c10afa27e9c2aa6657e8d4257705457a816a9
James R. Lambert <ryolambert@gmail.com>
2018-09-03 21:00:51 -0600

    sketching out the general map/list view layout

--- static-ui/explore-page.php
+++ static-ui/explore-page.php
@@ -1,6 +1,15 @@
 <?php require_once ("head-utils.php");?>
 <?php require_once ("navbar.php");?>
 
+<div class="map-list-container">
+	<div class="col-map">
+		<p>Map</p>
+	</div>
+	<div class="col-list">
+		<p>List</p>
+	</div>
+</div>
 
+<button class="btn btn-primary">List/Map</button>
 
 <?php require_once ("footer.php"); ?>
--- static-ui/js/script.js
+++ static-ui/js/script.js
@@ -1,1 +1,13 @@
-
+$('.panel-title').click(function() {
+	$(this)
+	// Find parent with the class that starts with "col-md"
+	// Change class to "col-md-3"
+		.closest('[class^="col-md"]')
+		.toggleClass('col-md-2 col-md-3')
+		// Find siblings of parent with similar class criteria
+		// - if all siblings are the same, you can use ".siblings()"
+		// Change class to "col-md-2"
+		.siblings('[class^="col-md"]')
+		.removeClass('col-md-3')
+		.addClass('col-md-2');
+});
