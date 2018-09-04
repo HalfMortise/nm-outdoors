b263e0a815df043485fb8e39ecec24b4f4facc15
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-03 22:22:04 -0500

    added rec area service

--- src/app/shared/services/rec.area.service.ts
+++ src/app/shared/services/rec.area.service.ts
@@ -1,1 +1,14 @@
-...
+import {Injectable} from "@angular/core";
+import {RecArea} from "../interfaces/rec.area";
+import {Observable} from "rxjs";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Injectable()
+
+export class RecAreaService {
+	constructor(protected http : HttpClient) {}
+	//api endpoint
+	private recAreaUrl = "api/recArea/";
+
+	//call the recArea API and get rec area object by its id
+}
