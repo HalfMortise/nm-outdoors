0bf0dab065c022dca33a23497f742cb30e1c695c
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 15:38:55 -0500

    updated review-post.php file with textarea and rating system

--- static-ui/review-post.php
+++ static-ui/review-post.php
@@ -1,16 +1,24 @@
 <?php require_once ("head-utils.php");?>
 <?php require_once "navbar.php"; ?>
 
+<div>
+<h3>Rating</h3>
+<p>Please leave a rating from 1-5 for this rec Area</p>
+</div>
+<div>
+	<h3>Review</h3>
+	<p>Please leave a review below</p>
+<form action="#">
+	<textarea name="review" id="review" cols="30" rows="10">
 
+	</textarea>
 
+</form>
+<button type="submit">Submit</button>
+<button type="submit">Edit</button>
+<button type="submit">Delete</button>
+<button type="submit">Reset</button>
 
-
-
-
-
-
-
-
-
+</div>
 
 <?php require_once "footer.php"; ?>
