99ac3d4bbaeb131f38f3fb881ab9e268dd9d4faf
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 14:22:36 -0500

    added profile-page.php to static ui

---
+++ static-ui/profile-page.php
@@ -0,0 +1,36 @@
+<?php require_once ("head-utils.php");?>
+<?php require_once ("navbar.php");?>
+
+<section>
+	<div id="profile">
+		<h3>My Profile</h3>
+		<ul>
+			<li>UserName</li>
+			<li>Email</li>
+		</ul>
+		<button type="button">Edit Profile</button>
+		<button type="button">Delete Profile</button>
+		<button type="button">Save Profile</button>
+		</div>
+</section>
+<section>
+   <div id="profilephoto">
+   <h3>My Photo/Avatar</h3>
+		<img src="" alt="profile image for user">
+	</div>
+</section>
+<section>
+	<div id="review">
+		<h3>My Reviews</h3>
+		<ul>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+			<li>Review goes here</li>
+		</ul>
+	</div>
+</section>
+
+<?php require_once "footer.php"; ?>
