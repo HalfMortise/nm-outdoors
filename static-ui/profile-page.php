a4287645db97030173bc9acece5560bffee8f63f
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 14:36:28 -0500

    added profile-photo.php to static ui

--- static-ui/profile-page.php
+++ static-ui/profile-page.php
@@ -13,12 +13,7 @@
 		<button type="button">Save Profile</button>
 		</div>
 </section>
-<section>
-   <div id="profilephoto">
-   <h3>My Photo/Avatar</h3>
-		<img src="" alt="profile image for user">
-	</div>
-</section>
+
 <section>
 	<div id="review">
 		<h3>My Reviews</h3>
---
+++ static-ui/profile-photo.php
@@ -0,0 +1,9 @@
+
+<section>
+   <div id="profilephoto">
+   <h3>My Photo/Avatar</h3>
+		<img src="" alt="profile image for user">
+	</div>
+	<button type="button">Upload Photo</button>
+	<button type="button">Delete Photo</button>
+</section>
