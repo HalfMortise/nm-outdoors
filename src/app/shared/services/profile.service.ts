10d956ceaad6e760b54159f4ff5241b4e85c246d
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 10:28:22 -0500

    added a profile service

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -1,1 +1,16 @@
-...
+import {Injectable} from "@angular/core";
+import {Profile} from "../interfaces/profile";
+import {Observable} from "rxjs";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Injectable()
+
+export class ProfileService {
+	constructor(protected http: HttpClient) {}
+	//api endpoint
+	private profileUrl = "api/Profile/";
+
+	//call to the Profile API and get profile object by its id
+
+
+}
