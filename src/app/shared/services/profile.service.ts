aa8dd3d4e248f51d90473a76332aaa904380468d
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-04 10:58:51 -0500

    updated profile service with createProfile method

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from "@angular/core";
+import {Status} from "../interfaces/status";
 import {Profile} from "../interfaces/profile";
 import {Observable} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
@@ -12,12 +13,17 @@ export class ProfileService {
 
 	//call to the Profile API and get profile object by its id
 getProfileByProfileId(profileId: string) : Observable<Profile>{
-	return(this.http.get<Profile>(this.profileUrl, {params: new HttpParams().set("id", profileId)}))
+	return(this.http.get<Profile>(this.profileUrl, {params: new HttpParams().set("id", profileId)}));
 }
 
 //call to the Profile API and get profile object by profile email
 	getProfileByProfileEmail(profileEmail: string) : Observable<Profile>{
-		return(this.http.get<Profile>(this.profileUrl, {params: new HttpParams().set("email", profileEmail )}))
+		return(this.http.get<Profile>(this.profileUrl, {params: new HttpParams().set("email", profileEmail )}));
+	}
+
+	//call to the Profile API and create a new profile
+	createProfile(profile: Profile) : Observable<Status>{
+		return(this.http.post<Status>(this.profileUrl, profile));
 	}
 
 }
