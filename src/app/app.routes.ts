03b2af16530bcf0728d7cc5e9de63ae7d12d549e
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-05 14:33:24 -0600

    fixed app.routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,17 @@ import {RouterModule, Routes} from "@angular/router";
 // import {SplashComponent} from "./splash/splash.component";
 // import {UserService} from "./shared/services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
+import {HomeComponent} from "./home/home.components";
 
 
-export const allAppComponents = [SplashComponent];
+export const allAppComponents = [HomeComponent];
 
 export const routes: Routes = [
-	{path: "", component: SplashComponent}
+	{path: "", component: HomeComponent}
 ];
 
 export const appRoutingProviders: any[] = [
 	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
-	UserService
 ];
 
 export const routing = RouterModule.forRoot(routes);
