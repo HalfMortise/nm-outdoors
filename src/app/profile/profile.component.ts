0a18520a302161818d4a3d222316dde6c63c849e
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-07 09:49:33 -0600

    Adding profile component with descriptive doc block

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,9 @@
+/******************************************************************************************************/
+/*	Module name: profile.component.ts																							*/
+/* Module description: Behind-the-curtain components populating the front-end of the user's profile		*/
+/*	Author: HalfMortise																											*/
+/*	Date: 9/7/2018																													*/
+/******************************************************************************************************/
+
+/* Imports */
+
