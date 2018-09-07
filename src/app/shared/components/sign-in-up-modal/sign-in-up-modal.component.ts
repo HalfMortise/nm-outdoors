3bf81f9313cffab9a20d779a4d0a4bf820248949
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-07 11:59:43 -0600

    Commit-pushing more changes; unfinished

--- src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
+++ src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
@@ -1,5 +1,5 @@
 /******************************************************************************************************/
-/*	Module name: profile.component.ts																							*/
+/*	Module name: sign-in-up-modal.component.ts																			*/
 /* Module description: Modal component that enables a user to sign up for or sign into profile (page)	*/
 /*	Author: HalfMortise																											*/
 /*	Date: 9/7/2018																													*/
@@ -15,6 +15,8 @@ import {JwtHelperService} from "@auth0/angular-jwt";
 import {SignUpService} from "../../services/sign.up.service";
 import {SignUp} from "../../interfaces/sign.up";
 import {SignIn} from "../../interfaces/sign.in";
+import {Router} from "@angular/router";
+import {CookieService} from "ng2-cookies";
 
 
 /* Component */
@@ -38,6 +40,7 @@ export class SignInUpModalComponent implements OnInit{
 		this.currentlySignedIn()
 	}
 
+	//validates user's session when signing in
 	currentlySignedIn() : void {
 
 		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
@@ -50,3 +53,39 @@ export class SignInUpModalComponent implements OnInit{
 }
 
 
+@Component({
+	template: require( "./sign-in-up-modal.html"),
+	selector: "signinupmodal"
+})
+
+export class SignInComponent {
+	@ViewChild("signInForm") signInForm: any;
+
+	signin: SignIn = {profileEmail: null, profilePassword: null};
+	signup: SignUp = {profileAtHandle: null, profileEmail: null, profilePassword: null, profilePasswordConfirm: null}
+	status: Status = {status: null, type: null, message: null};
+
+	//cookie: any = {};
+	constructor(private SignInService: SignInService, private SignUpService: SignUpService, private router: Router, private cookieService : CookieService) {
+	}
+
+
+
+	signIn(): void {
+		localStorage.removeItem("jwt-token");
+		this.SignInService.postSignIn(this.signin).subscribe(status => {
+			this.status = status;
+
+			if(status.status === 200) {
+
+				this.router.navigate(["profile-page"]);
+
+				//location.reload(true);
+				this.signInForm.reset();
+				setTimeout(1000,function(){$("#sign-in-up-modal").modal('hide');});
+			} else {
+				console.log("failed login")
+			}
+		});
+	}
+}
