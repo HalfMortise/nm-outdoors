5395c730e37ed023049c869714b154f7bd906485
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-07 11:27:31 -0600

    Drafted sign in/sign up modal component; imported interfaces and services

--- src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
+++ src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
@@ -1,15 +1,52 @@
 /******************************************************************************************************/
 /*	Module name: profile.component.ts																							*/
-/* Module description: Behind-the-curtain components populating the front-end of the user's profile		*/
+/* Module description: Modal component that enables a user to sign up for or sign into profile (page)	*/
 /*	Author: HalfMortise																											*/
 /*	Date: 9/7/2018																													*/
 /******************************************************************************************************/
 
 /* Imports */
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {SignInService} from "../../services/sign.in.service";
+import {Status} from "../../interfaces/status";
+import {Profile} from "../../interfaces/profile";
+import {ProfileService} from "../../services/profile.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
+import {SignUpService} from "../../services/sign.up.service";
+import {SignUp} from "../../interfaces/sign.up";
+import {SignIn} from "../../interfaces/sign.in";
 
 
 /* Component */
+
 @Component({
-	template: require("./sign-in-up-modal.html")
+	template:`
+	<h1>{{profile.profileAtHandle}}</h1>
+	
+	`
 })
+export class SignInUpModalComponent implements OnInit{
+
+	profile: Profile;
+	status: Status;
+	signIn: SignIn;
+	signUp: SignUp;
+
+	constructor(private profileService: ProfileService, private jwtHelper : JwtHelperService, private signInService: SignInService, private signUpService: SignUpService) {}
+
+	ngOnInit(): void {
+		this.currentlySignedIn()
+	}
+
+	currentlySignedIn() : void {
+
+		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
+
+		this.profileService.getProfileByProfileEmail(decodedJwt.auth.profileId)
+			.subscribe(profile => this.profile = profile)
+
+	}
+
+}
+
+
