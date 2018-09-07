e9fe1af5ad202bcb057a7fa304d921edad27aa45
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-07 10:48:13 -0600

    Added doc block and component bones

--- src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
+++ src/app/shared/components/sign-in-up-modal/sign-in-up-modal.component.ts
@@ -1,1 +1,15 @@
-...
+/******************************************************************************************************/
+/*	Module name: profile.component.ts																							*/
+/* Module description: Behind-the-curtain components populating the front-end of the user's profile		*/
+/*	Author: HalfMortise																											*/
+/*	Date: 9/7/2018																													*/
+/******************************************************************************************************/
+
+/* Imports */
+import {Component} from "@angular/core";
+
+
+/* Component */
+@Component({
+	template: require("./sign-in-up-modal.html")
+})
