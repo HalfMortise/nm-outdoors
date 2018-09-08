1e67980ab1c073f6ac7049e450ae55fdea9ec63d
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-08 17:35:13 -0600

    Added descriptive doc block

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,1 +1,12 @@
-...
+/******************************************************************************************************/
+/*	Module name: about.component.ts																							*/
+/* Module description: Behind-the-curtain components populating the front-end of the about page			*/
+/*	Author: HalfMortise																											*/
+/*	Date: 9/8/2018																													*/
+/******************************************************************************************************/
+
+/* Imports */
+
+
+
+
