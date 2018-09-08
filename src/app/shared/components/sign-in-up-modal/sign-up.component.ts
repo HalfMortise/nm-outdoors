c7f4e4a685c6f216084ace6f38081595a4385ee2
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-08 11:17:49 -0600

    Going at this from a different angle: separated sign in/sign up components

---
+++ src/app/shared/components/sign-in-up-modal/sign-up.component.ts
@@ -0,0 +1,8 @@
+/******************************************************************************************************/
+/*	Module name: sign-up.component.ts																							*/
+/* Module description: Component of a modal that enables a user to sign up for a profile (page)			*/
+/*	Author: HalfMortise																											*/
+/*	Date: 9/8/2018																													*/
+/******************************************************************************************************/
+
+/* Imports */
