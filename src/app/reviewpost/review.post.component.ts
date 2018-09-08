3571e9a03b3a00079f3774b99b9fdcc9d79c72fd
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-07 21:21:30 -0500

    created ReviewPostComponent class and constructor

---
+++ src/app/reviewpost/review.post.component.ts
@@ -0,0 +1,29 @@
+/* imports*/
+
+import {Component, OnInit} from "@angular/core";
+import {ReviewService} from "../shared/services/review.service";
+import {Review} from "../shared/interfaces/review";
+import {Observable} from "rxjs";
+
+
+/*component*/
+
+@Component({
+	template: require("./reviewpost.html"),
+	selector: "review-post"
+
+})
+
+export class ReviewPostComponent implements OnInit{
+	reviews: Review;
+
+	constructor(){
+
+
+
+	}
+	ngOnInit(){
+
+	}
+
+}
