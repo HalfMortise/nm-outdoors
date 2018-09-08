12cc261c67febaf75dfe3eedb41c119008368af2
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-08 17:53:30 -0500

    added code to constructor, ngOnInit and loadReview to the component

--- src/app/reviewpost/review.post.component.ts
+++ src/app/reviewpost/review.post.component.ts
@@ -1,10 +1,14 @@
 /* imports*/
 
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import {ReviewService} from "../shared/services/review.service";
 import {Review} from "../shared/interfaces/review";
 import {Observable} from "rxjs";
-
+import {RecAreaService} from "../shared/services/rec.area.service";
+import {RecArea} from "../shared/interfaces/rec.area";
+import {ActivatedRoute, Params} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Status} from "../shared/interfaces/status";
 
 
 /*component*/
@@ -16,14 +20,27 @@ import {Observable} from "rxjs";
 })
 
 export class ReviewPostComponent implements OnInit{
-	reviews: Review;
+	reviews: Review[] = [];
+	recAreaId = this.route.snapshot.params["recAreaId"];
+	recArea: RecArea;
+	status: Status;
 
-	constructor(protected reviewService: ReviewService){
+
+	constructor(protected reviewService: ReviewService, protected recAreaService: RecAreaService, protected route: ActivatedRoute, protected formBuilder: FormBuilder){
 
 
 
 	}
 	ngOnInit(){
+		this.recAreaService.getRecAreaByRecAreaId(this.recAreaId).subscribe(recArea => this.recArea = recArea);
+		this.createAreaForm = this.formBuilder.group({
+			reviewText: ["",[Validators.maxLength(512), Validators.required]]
+		} );
+   this.loadReviews();
+	}
+
+	loadReviews(): any {
+		this.reviewService.getReviewbyRecAreaId(this.recAreaId).subscribe(reviews => this.reviews = reviews);
 
 	}
 
