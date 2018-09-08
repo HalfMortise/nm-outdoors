db865e2a8f79c9b460fb764496db0591278f2939
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-08 11:33:55 -0600

    Added import statements and component content

--- src/app/shared/components/sign-in-up-modal/sign-in.component.ts
+++ src/app/shared/components/sign-in-up-modal/sign-in.component.ts
@@ -6,3 +6,47 @@
 /******************************************************************************************************/
 
 /* Imports */
+
+import {Component, ViewChild} from "@angular/core";
+import {SignIn} from "../../interfaces/sign.in";
+import {Status} from "../../interfaces/status";
+import {SignInService} from "../../services/sign.in.service";
+import {Router} from "@angular/router";
+import {CookieService} from "ng2-cookies";
+
+declare var $: any;
+
+@Component({
+	template: require( "./sign-in-up-modal.html"),
+	selector: "signin"
+})
+
+export class SignInComponent {
+	@ViewChild("signInForm") signInForm: any;
+
+	signin: SignIn = {profileEmail: null, profilePassword: null};
+	status: Status = {status: null, type: null, message: null};
+	//cookie: any = {};
+	constructor(private SignInService: SignInService, private router: Router, private cookieService : CookieService) {
+	}
+
+
+
+	signIn(): void {
+		localStorage.removeItem("jwt-token");
+		this.SignInService.postSignIn(this.signin).subscribe(status => {
+			this.status = status;
+
+
+			if(status.status === 200) {
+
+				this.router.navigate(["profile-page"]);
+				//location.reload(true);
+				this.signInForm.reset();
+				setTimeout(1000,function(){$("#sign-in-up-modal").modal('hide');});
+			} else {
+				console.log("failed login")
+			}
+		});
+	}
+}
