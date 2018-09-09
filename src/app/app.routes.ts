796422e3419cf0637b90dbb9bbe2975efb88720a
Bashir Shafii <bashir.shafii@gmail.com>
2018-09-09 14:51:29 -0500

    added ReviewPostComponent to the app.routes.ts export array list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,12 +20,14 @@ import {ProfileComponent} from "./profile/profile.component";
 import {RecAreaListComponent} from "./explore/recArea-list.component";
 import {SignInComponent} from "./shared/components/sign-in-up-modal/sign-in.component";
 import {SignUpComponent} from "./shared/components/sign-in-up-modal/sign-up.component";
+import {ReviewPostComponent} from "./reviewpost/review.post.component";
 
 
 
-export const allAppComponents = [HomeComponent, ExploreComponent, RecAreaModalComponent, ProfileComponent, RecAreaListComponent, SignInComponent, SignUpComponent];
+export const allAppComponents = [HomeComponent, ExploreComponent, RecAreaModalComponent, ProfileComponent, RecAreaListComponent, SignInComponent, SignUpComponent,ReviewPostComponent];
 
 export const routes: Routes = [
+	{path:"review/:reviewRecAreaId", component: ReviewPostComponent},
 	{path: "recAreaModal/:recAreaId", component: RecAreaModalComponent},
 	{path: "recArea", component: RecAreaListComponent},
 	{path: "explore", component: ExploreComponent},
