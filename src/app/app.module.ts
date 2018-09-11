c77734296fb844e692f4403d0cf895b552474c31
James R. Lambert <ryolambert@gmail.com>
2018-09-11 13:49:23 -0600

    Fixed module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AngularSplitModule } from 'angular-split';
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NguiMapModule} from "@ngui/map";
 import {NgxPaginationModule} from "ngx-pagination";
 
@@ -14,7 +14,15 @@ import {NgxPaginationModule} from "ngx-pagination";
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [ AngularSplitModule, BrowserModule, HttpClientModule, NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyBMQE2mPIzXsRIbSUWzBUwiJrdrp80Xkqc'}), routing, ReactiveFormsModule, NgxPaginationModule],
+	imports:      [
+		AngularSplitModule,
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyBMQE2mPIzXsRIbSUWzBUwiJrdrp80Xkqc'}),
+		routing,
+		ReactiveFormsModule,
+		NgxPaginationModule],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
