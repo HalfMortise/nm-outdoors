f605cd3e2ff79d42f30aeb8a518e084263a61a1d
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-11 14:36:49 -0600

    Did some work on the review-post form; lots of progress, but still needs some work - not yet functional

--- src/app/reviewpost/review-post.component.ts
+++ src/app/reviewpost/review-post.component.ts
@@ -14,7 +14,7 @@ import {Status} from "../shared/interfaces/status";
 /*component*/
 
 @Component({
-	template: require("./reviewpost.template.html"),
+	template: require("./review-post.template.html"),
 	selector: "review-post"
 
 })
