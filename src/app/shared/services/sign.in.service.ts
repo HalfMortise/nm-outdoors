5ce4ce3b1a5988a74528c820ec9d59c1062cc745
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-11 10:06:38 -0600

    Created separate sign-out service; updated sign-in service to reflect changes

--- src/app/shared/services/sign.in.service.ts
+++ src/app/shared/services/sign.in.service.ts
@@ -7,19 +7,13 @@ import {SignIn} from "../interfaces/sign.in";
 
 @Injectable()
 export class SignInService {
-	constructor(protected http : HttpClient) {
-
-	}
+	constructor(protected http : HttpClient) {}
 
 	private signInUrl = "api/sign-in/";
-	private signOutUrl = "api/sign-out/";
 
 	//perform the post to initiate sign in
 	postSignIn(signIn:SignIn) : Observable<Status> {
 		return(this.http.post<Status>(this.signInUrl, signIn));
 	}
 
-	signOut() : Observable<Status> {
-		return(this.http.get<Status>(this.signOutUrl))
-	}
 }
