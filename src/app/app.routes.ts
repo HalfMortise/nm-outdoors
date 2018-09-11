729c510d680600dded1c9c2db389cab97d026134
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-11 13:32:08 -0600

    continued work on navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import {RouterModule, Routes} from "@angular/router";
 // import {SplashComponent} from "./splash/splash.component";
 // import {UserService} from "./shared/services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
-
-/*Service Imports*/
+import {HomeComponent} from "./home/home.component";
+import {ExploreComponent} from "./explore/explore.component";
 import {ActivityService} from "./shared/services/activity.service";
 import {RecAreaService} from "./shared/services/rec.area.service";
 import {ReviewService} from "./shared/services/review.service";
@@ -13,32 +13,28 @@ import {SignUpService} from "./shared/services/sign.up.service";
 import {ActivationService} from "./shared/services/activation.service";
 import {AuthService} from "./shared/services/auth.service";
 import {ProfileService} from "./shared/services/profile.service";
-
-/*Interceptor Imports*/
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {DeepDiveInterceptor} from "./shared/interceptors/deep.dive.interceptor";
-
-/*Component Imports*/
-import {HomeComponent} from "./home/home.component";
-import {ExploreComponent} from "./explore/explore.component";
 import {RecAreaModalComponent} from "./explore/recArea-modal/recArea-modal.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {RecAreaListComponent} from "./explore/recArea-list/recArea-list.component";
-import {SignInComponent} from "./shared/components/sign-in/sign-in.component";
-import {SignUpComponent} from "./shared/components/sign-up/sign-up.component";
+// import {SignInComponent} from "./shared/components/sign-in/sign-in.component";
+// import {SignUpComponent} from "./shared/components/sign-up/sign-up.component";
 import {ReviewPostComponent} from "./reviewpost/review.post.component";
 
 
 
-export const allAppComponents = [HomeComponent, ExploreComponent, RecAreaModalComponent, ProfileComponent, RecAreaListComponent, SignInComponent, SignUpComponent, ReviewPostComponent];
+export const allAppComponents = [HomeComponent, ExploreComponent, RecAreaModalComponent, ProfileComponent, RecAreaListComponent, ReviewPostComponent];
 
 export const routes: Routes = [
+	// {path: }
+	// {path: "about", component: AboutComponent},
 	{path:"review/:reviewRecAreaId", component: ReviewPostComponent},
 	{path: "recAreaModal/:recAreaId", component: RecAreaModalComponent},
 	{path: "recArea", component: RecAreaListComponent},
 	{path: "explore", component: ExploreComponent},
-	{path: "sign-up-modal", component: SignUpComponent},
-	{path: "sign-in", component: SignInComponent},
+	// {path: "sign-in-modal", component: SignUpComponent},
+	// {path: "sign-in-modal", component: SignInComponent},
 	{path: "", component: HomeComponent}
 ];
 const services : any[] = [ActivationService, ActivityService, AuthService, ProfileService, RecAreaService, ReviewService, SessionService, SignInService, SignUpService];
