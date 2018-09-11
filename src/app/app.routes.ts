81b0b6153fc52799079014719cf38847ab008e2c
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-11 11:00:38 -0600

    Categorized routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,8 @@ import {RouterModule, Routes} from "@angular/router";
 // import {SplashComponent} from "./splash/splash.component";
 // import {UserService} from "./shared/services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
-import {HomeComponent} from "./home/home.component";
-import {ExploreComponent} from "./explore/explore.component";
+
+/*Service Imports*/
 import {ActivityService} from "./shared/services/activity.service";
 import {RecAreaService} from "./shared/services/rec.area.service";
 import {ReviewService} from "./shared/services/review.service";
@@ -13,8 +13,14 @@ import {SignUpService} from "./shared/services/sign.up.service";
 import {ActivationService} from "./shared/services/activation.service";
 import {AuthService} from "./shared/services/auth.service";
 import {ProfileService} from "./shared/services/profile.service";
+
+/*Interceptor Imports*/
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {DeepDiveInterceptor} from "./shared/interceptors/deep.dive.interceptor";
+
+/*Component Imports*/
+import {HomeComponent} from "./home/home.component";
+import {ExploreComponent} from "./explore/explore.component";
 import {RecAreaModalComponent} from "./explore/recArea-modal/recArea-modal.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {RecAreaListComponent} from "./explore/recArea-list/recArea-list.component";
@@ -31,8 +37,8 @@ export const routes: Routes = [
 	{path: "recAreaModal/:recAreaId", component: RecAreaModalComponent},
 	{path: "recArea", component: RecAreaListComponent},
 	{path: "explore", component: ExploreComponent},
-	{path: "sign-in-modal", component: SignUpComponent},
-	{path: "sign-in-modal", component: SignInComponent},
+	{path: "sign-up-modal", component: SignUpComponent},
+	{path: "sign-in", component: SignInComponent},
 	{path: "", component: HomeComponent}
 ];
 const services : any[] = [ActivationService, ActivityService, AuthService, ProfileService, RecAreaService, ReviewService, SessionService, SignInService, SignUpService];
