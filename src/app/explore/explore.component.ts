8cdc84c5dd8d6799061e44e293206b7d76094978
James R. Lambert <ryolambert@gmail.com>
2018-09-12 15:10:55 -0600

    added link routes to specific areas.

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -6,9 +6,10 @@
  **/
 
 import {Component, OnInit, ViewChild} from "@angular/core";
+import {Status} from "../shared/interfaces/status";
 import {RecArea} from "../shared/interfaces/rec.area";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Route, Router} from "@angular/router";
 import {RecAreaService} from "../shared/services/rec.area.service";
 import {RecAreaModalComponent} from "./recArea-modal/recArea-modal.component";
 
@@ -22,10 +23,20 @@ export class ExploreComponent implements OnInit{
 	recAreaSearchForm : FormGroup;
 	detailedRecArea : RecArea = {recAreaId : null, recAreaDescription : "", recAreaDirections : "", recAreaImageUrl : "", recAreaLat : "", recAreaLong : "", recAreaMapUrl : "", recAreaName : ""};
 	direction: string = 'horizontal';
+	recAreaParameter: string;
+	recAreaValue: string;
+  status: Status = {status: null, message: null, type: null};
+  searchParameters : any[] = [
+    {"parameter" : "name"},
+    // {"parameter" : "activity"},
+		{"parameter" : "description"},
+    {"parameter" : "status"},
+  ];
 
 	@ViewChild(RecAreaModalComponent) recAreaModalComponent : RecAreaModalComponent;
 
 	constructor(private recAreaService: RecAreaService, private router: Router, private formBuilder: FormBuilder){
+		router.onSameUrlNavigation = "reload";
 	}
 
 	ngOnInit() : void {
@@ -33,6 +44,7 @@ export class ExploreComponent implements OnInit{
 		this.recAreaSearchForm = this.formBuilder.group({
 			recAreaSearchName: ["",[Validators.maxLength(140), Validators.minLength(1)]]
 		});
+		// this.loadSearchResults();
 	}
 
 	showRecAreas() : void {
@@ -48,6 +60,60 @@ export class ExploreComponent implements OnInit{
 		this.detailedRecArea = detailedRecArea;
 		this.recAreaModalComponent.recArea = detailedRecArea;
 	}
+
+	//Search Functions
+
+  // getSearchResults() {
+  //   let searchParameter = this.recAreaSearchForm.value.searchParameter;
+  //   let searchContent = this.recAreaSearchForm.value.searchContent;
+  //   this.router.navigate(["/search", "explore" + searchParameter.charAt(0).toUpperCase() + searchParameter.substring(1), searchContent])
+  //     .then(() => this.loadSearchResults());
+  //   // this.router.navigate(["search", this.searchForm.value.searchParameter, this.searchForm.value.searchContent])
+  // }
+	//
+  // loadSearchResults() {
+  //   this.recAreaParameter = this.route.snapshot.params["recAreaParameter"];
+  //   this.recAreaValue = this.route.snapshot.params["recAreaValue"];
+  //   if(this.recAreaParameter === "recAreaStatus"){
+  //   	this.loadStatus(this.recAreaValue);
+	// 	} else if(this.)
+  //   // if(this.recAreaParameter === "recAreaStatus"){
+  //   //   this.loadStatus(this.recAreaValue);
+  //   // } else if(this.recAreaParameter === "recAreaActivity"){
+  //   //   this.loadActivity(this.recAreaValue);
+  //   // } else if(this.recAreaParameter === "recAreaGender"){
+  //   //   this.loadGender(this.recAreaValue);
+  //   // } else if(this.recAreaParameter === "recAreaSpecies"){
+  //   //   this.loadSpecies(this.recAreaValue);
+  //   // }
+  // }
+	//
+  // // loadActivity(recAreaActivity: string){
+  // //   this.recAreaService.getRecAreaByRecAreaActivity(recAreaActivity).subscribe(recArea => this.recArea = recAreas);
+  // // }
+	//
+  // loadGender(recAreaGender: string){
+  //   this.recAreaService.getrecAreaByrecAreaGender(recAreaGender).subscribe(recAreas => this.recAreas = recAreas);
+  // }
+	//
+  // loadSpecies(recAreaSpecies: string){
+  //   if(recAreaSpecies === "8472") {
+  //     alert("THE WEAK SHALL PERISH");
+  //   }
+  //   this.recAreaService.getrecAreaByrecAreaSpecies(recAreaSpecies).subscribe(recAreas => this.recAreas = recAreas);
+  // }
+	//
+  // loadStatus(recAreaStatus: string){
+  //   this.recAreaService.getrecAreaByrecAreaStatus(recAreaStatus).subscribe(recAreas => this.recAreas = recAreas);
+  // }
+
+  getRecAreaByRecAreaName() : void {
+
+    this.recAreaService.getRecAreaByRecAreaName(this.recAreaSearchForm.value.recAreaName).subscribe(reply =>{
+      this.recAreas=reply;
+      this.recAreaSearchForm.reset();
+    });
+  }
 
 	// getAllRecAreas() : void {
 	// 	this.recAreaService.getAllRecAreas().subscribe(reply =>{
--- src/app/ratepost/rate.post.component.ts
+++ src/app/ratepost/rate.post.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
-	selector: 'app-rating',
+	selector: 'rate-post',
 	templateUrl: './ratepost.template.html',
 
 })
