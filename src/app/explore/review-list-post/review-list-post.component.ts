bca85a054822c87a6e0b11df332e3576d2641f9a
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-13 14:28:41 -0600

    made component for reviews. Still WIP

---
+++ src/app/explore/review-list-post/review-list-post.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from "@angular/core";
+import {RecAreaService} from "../shared/services/rec.area.service";
+import {ActivatedRoute, Route, Router} from "@angular/router";
+import {RecArea} from "../shared/interfaces/rec.area";
+import {ProfileService} from "../../shared/services/profile.service";
+import {Review} from "../../shared/interfaces/review";
+import {ReviewService} from "../../shared/services/review.service";
+
+@Component({
+	template: require("./review-list-post.html")
+})
+
+export class RecAreaListComponent implements OnInit {
+	@Input() recAreaId: string;
+	recAreas: RecArea[] = [];
+	recArea: RecArea;
+	reviews: Review[] = [];
+	review: Review;
+
+
+
+	constructor(
+		protected recAreaService: RecAreaService,
+		protected reviewService: ReviewService,
+		protected profileService: ProfileService,
+		protected route: ActivatedRoute
+	) {
+	}
+
+	ngOnInit() {
+		this.loadReviews();
+	}
+
+	loadReviews(): any {
+		this.recAreaService.getAllReviews() . subscribe(reviews => this.reviews = reviews);
+	}
+}
