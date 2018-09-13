aaae927193f432adf3b8acbe9267c0435788183b
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-13 16:42:06 -0600

    Fixed the search to be able to search by activityType! Hurrah!

--- src/app/explore/recArea-list/recArea-list.component.ts
+++ src/app/explore/recArea-list/recArea-list.component.ts
@@ -1,17 +1,10 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {RecAreaService} from "../../shared/services/rec.area.service";
 import {RecArea} from "../../shared/interfaces/rec.area";
-import {ActivatedRoute} from "@angular/router";
 import {NgxPaginationModule} from 'ngx-pagination';
 import {Status} from "../../shared/interfaces/status";
 import {Activity} from "../../shared/interfaces/activity";
 
-
-// @module({
-// 	imports: [BrowserModule, NgxPaginationModule], // <-- include it in your app module
-// 	declarations: [RecAreaListComponent],
-// 	bootstrap: [RecAreaListComponent]
-// })
 
 @Component ({
 	selector: "recArea-list",
@@ -20,32 +13,13 @@ import {Activity} from "../../shared/interfaces/activity";
 })
 
 export class RecAreaListComponent implements OnInit {
-	@Input() recAreaId: string;
+	@Input()
 	recAreas: RecArea[] = [];
-	recArea: RecArea;
-	activity: Activity = {activityId: null, activityName: null};
 	activities: Activity[] =[];
-	// searchForm: FormGroup;
-	searchValue: string;
 	status: Status = {status: null, message: null, type: null};
-	searchParameters: any [] = [
-		{"parameter": "Wildlife Viewing"},
-		{"parameter": "Fishing"},
-		{"parameter": "Hiking"},
-		{"parameter": "Camping"},
-		{"parameter": "Water Sports"},
-		{"parameter": "Biking"},
-		{"parameter": "Winter Sports"},
-	];
-	activityParameter: string;
-	activityValue: string;
-	activityId = this.route.snapshot.params["activityId"];
-
-
 
 	constructor(
-		protected recAreaService: RecAreaService,
-		protected route: ActivatedRoute
+		protected recAreaService: RecAreaService
 	) {
 	}
 
