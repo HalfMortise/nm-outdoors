1240d86099f6edeb480816d39e890c066334d893
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-13 16:21:19 -0600

    removed path for double stack

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -44,7 +44,6 @@ export const routes: Routes = [
 	{path: "sign-up-modal", component: SignUpComponent},
 	{path: "sign-in", component: SignInComponent},
 	{path: "profile", component: ProfileComponent},
-	{path: "home/:activityParameter/:activityValue", component: HomeComponent},
 	{path: "", component: HomeComponent}
 ];
 const services : any[] = [ActivationService, ActivityService, AuthService, ProfileService, RecAreaService, ReviewService, SessionService, SignInService, SignUpService];
