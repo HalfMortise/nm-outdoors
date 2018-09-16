1ea75b252eaf950a90c37ec72a6da52a88d8d5cc
Sarah Heckendorn <praisinghim@jcangus.com>
2018-09-16 11:16:19 -0600

    removed profile sections from here as well

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -44,8 +44,6 @@ export class ProfileComponent implements OnInit {
 		reviewContent: "",
 		reviewDateTime: "",
 		reviewRating: null,
-		profileAtHandle: null,
-		profileImageUrl: null
 	};
 
 	recArea: RecArea;
