b76fcec8e133ea58447bdaa468b16cf632eb0d34
Half Mortise <HalfMortise@users.noreply.github.com>
2018-09-18 12:38:12 -0600

    Removed sign-out service from sign.in service; still working on this functionality

--- src/app/shared/services/sign.in.service.ts
+++ src/app/shared/services/sign.in.service.ts
@@ -10,10 +10,15 @@ export class SignInService {
 	constructor(protected http : HttpClient) {}
 
 	private signInUrl = "api/sign-in/";
+	private signOutUrl = "api/sign-out/";
 
 	//perform the post to initiate sign in
 	postSignIn(signIn:SignIn) : Observable<Status> {
 		return(this.http.post<Status>(this.signInUrl, signIn));
+	}
+
+	signOut() : Observable<Status> {
+		return(this.http.get<Status>(this.signOutUrl));
 	}
 
 }
